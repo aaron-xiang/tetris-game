d4a7080274d9ba633f81203085ba3355454f5085
Henry Xiang <henryxiang@gmail.com>
2024-09-14 18:33:57 -0400

    bugfix: webassembly build artifact not working

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "colors.h"
 #include <iostream>
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
 double lastUpdateTime = 0;
 Font font;
 Game game;
@@ -48,62 +52,17 @@ void UpdateDrawFrame() {
 
 int main() {
     InitWindow(500, 620, "raylib tetris");
-    SetTargetFPS(60);
-
     font = LoadFontEx("font/monogram.ttf", 64, 0, 0);
     game = Game();
 
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);
     // Main loop control (controlled by browser or platform)
     while (!WindowShouldClose()) {
         UpdateDrawFrame();
     }
-
+#endif
     CloseWindow();
 }
-
-// double lastUpdateTime = 0;
-
-// bool EventTriggered(double interval) {
-//   double currentTime = GetTime();
-//   if (currentTime - lastUpdateTime >= interval) {
-//     lastUpdateTime = currentTime;
-//     return true;
-//   }
-//   return false;
-// }
-
-// int main()
-// {
-//   InitWindow(500, 620, "raylib tetris");
-//   SetTargetFPS(60);
-
-//   Font font = LoadFontEx("font/monogram.ttf", 64, 0, 0);
-
-//   Game game = Game();
-
-//   while(WindowShouldClose() == false) {
-//     game.HandleInput();
-//     if (EventTriggered(0.2)) {
-//       game.MoveBlockDown();
-//     }
-//     BeginDrawing();
-//     ClearBackground(darkBlue);
-//     DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
-//     DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
-//     if (game.gameOver) {
-//       DrawTextEx(font, "Game Over", {320, 450}, 38, 2, WHITE);
-//     }
-//     DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
-
-//     char scoreText[10];
-//     sprintf(scoreText, "%d", game.score);
-//     Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
-
-//     DrawTextEx(font, scoreText, {320 + (170 - textSize.x)/2, 65}, 38, 2, WHITE);
-//     DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
-//     game.Draw();
-//     EndDrawing();
-//   }
-
-//   CloseWindow();
-// }
